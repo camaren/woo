7788de8245945b76abc4f6ec3f90c9f850f69fb5
Camaren <camaren@Gabriels-MacBook-Pro.local>
2017-04-23 17:58:44 -0400

    and another one

---
+++ cmake-build-debug/CMakeFiles/woo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/woo.dir/GenericInequalities.cpp.o"
+  "CMakeFiles/woo.dir/TestGenericInequalities.cpp.o"
+  "woo.pdb"
+  "woo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/woo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
